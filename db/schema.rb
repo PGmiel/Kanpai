886023293218cc67875154e6f3651fb59f89a7fc
Kyrst1 <kyrstinlegrand@hotmail.fr>
2021-06-08 15:14:31 +0200

    dahsboard added for owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_093217) do
+ActiveRecord::Schema.define(version: 2021_06_08_123426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_093217) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "status_owner" #t.boolean "status-owner", default => false linked to devise/registration/check box
+    t.boolean "status_owner", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
