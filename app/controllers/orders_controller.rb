40b247fa08b9f9ba999da449d192d29c5744ced4
Roch de Vidts <roch-dv@hotmail.com>
2021-06-09 17:10:57 +0200

    Added all my heart to the show page of Order, need to finish the paying button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,17 @@
 class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_items = OrderItem.all
+    @order_item = OrderItem.new
     @table = Table.find(@order.table_id)
     @restaurant = Restaurant.find(@table.restaurant_id)
-    @drinks_category = ["Hot Drinks", "Beers", "Wine"]
-    @food_category = ["Starter", "Main Dishes", "Dessert"]
+    # @drinks_category = ["Hot Drinks", "Beers", "Wine"]
+    # @food_category = ["Starter", "Main Dishes", "Dessert"]
+    @starters = @restaurant.starters
+    @main_dishes = @restaurant.main_dishes
+    @desserts = @restaurant.desserts
+    @hot_drinks = @restaurant.hot_drinks
+    @beers = @restaurant.beers
+    @wines = @restaurant.wines
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,7 +7,7 @@ class Restaurant < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  
+
   include PgSearch::Model
   pg_search_scope :general_search,
     against: [:name, :address],
@@ -21,5 +21,41 @@ class Restaurant < ApplicationRecord
   def find_table(datetime, number_of_customers)
     available_tables = tables.where("capacity >= ?", number_of_customers).order(:capacity)
     available_table = available_tables.detect { |table| table.bookings.where(datetime: datetime).empty? }
+  end
+
+  def starters
+    self.menu_items.select do |menu_item|
+      menu_item.category == "Starter"
+    end
+  end
+
+  def main_dishes
+    self.menu_items.select do |menu_item|
+      menu_item.category == "Main Dishes"
+    end
+  end
+
+  def desserts
+    self.menu_items.select do |menu_item|
+      menu_item.category == "Dessert"
+    end
+  end
+
+  def hot_drinks
+    self.menu_items.select do |menu_item|
+      menu_item.category == "Hot Drinks"
+    end
+  end
+
+  def beers
+    self.menu_items.select do |menu_item|
+      menu_item.category == "Beers"
+    end
+  end
+
+  def wines
+    self.menu_items.select do |menu_item|
+      menu_item.category == "Wine"
+    end
   end
 end
