fe5f9b17ec34e8cde657961b1bc32f1e4f230647
Pierre-Guillaume <80978648+PGmiel@users.noreply.github.com>
2021-06-14 12:17:21 +0200

    fixed the booking system, still need to work on the booking status update

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -7,7 +7,7 @@ class TablesController < ApplicationController
     @bookings = Booking.all
     @table_bookings = @bookings.where(table_id: @table)
 
-    if check_if_booking(@table.id)
+    if @table.status == "available" && @table.check_if_booking.present?
       redirect_to validate_user_path(@table)
     elsif @order
       redirect_to order_path(@order)
@@ -20,27 +20,15 @@ class TablesController < ApplicationController
 
   def validate_user
     @table = Table.find(params[:table_id])
-    @check_user = user_booking(@table.id)
-    @user = User.find(@check_user.first.user_id)
+    @user = user_booking.first.full_name
   end
 
-  def check_if_booking(table_id)
-    @table = Table.find(table_id)
-    @table_bookings = Booking.where(table_id: @table)
-    @check_bookings = @table_bookings.select do |table_booking|
-      (table_booking.starts_at > DateTime.now) &&
-      (table_booking.starts_at < DateTime.now.advance(minutes: 30))
-    end
-    @check_bookings.present?
-  end
-
-  def user_booking(table_id)
-    @table = Table.find(table_id)
-    @table_bookings = Booking.where(table_id: @table)
+  def user_booking
+    @table_bookings = @table.bookings.where(status: "booked")
     @bookings = @table_bookings.select do |table_booking|
       (table_booking.starts_at > DateTime.now) &&
       (table_booking.starts_at < DateTime.now.advance(minutes: 30))
     end
-    @bookings
+    @bookings.map { |booking| booking.user }
   end
 end
