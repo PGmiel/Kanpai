9005053e9fc816e3bd226b2e61394358616e245d
Vincent Herremans <vincent.herremans@gmail.com>
2021-06-14 16:20:56 +0200

    navbar dropdown

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,13 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
+  if (document.querySelector("#banner-typed-text")) {
   new Typed('#banner-typed-text', {
     strings: ["Book a restaurant", "Book a terrasse seat", "Find an empty table near you"],
     typeSpeed: 50,
     loop: true
   });
+  }
 }
 
 export { loadDynamicBannerText };
