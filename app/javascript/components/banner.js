e33ce9de095b08d7f31c8e6c8cb44d141af77cae
Kyrst1 <kyrstinlegrand@hotmail.fr>
2021-06-14 17:23:22 +0200

    filter added on homepge

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Book a restaurant", "Book a terrasse seat", "Find an empty table near you"],
-    typeSpeed: 50,
+    typeSpeed: 54,
     loop: true
   });
 }
