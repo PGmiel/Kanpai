9a3bfd7276d0671676fae3018c55f6124c4867e7
Pierre-Guillaume <80978648+PGmiel@users.noreply.github.com>
2021-06-15 12:30:14 +0200

    done fixing booking

--- app/jobs/check_table_status_job.rb
+++ app/jobs/check_table_status_job.rb
@@ -3,6 +3,6 @@ class CheckTableStatusJob < ApplicationJob
 
   def perform(table_id)
     @table = Table.find(table_id)
-    @table.update(status: "booked") if @table.status == "available"
+    @table.update(status: "booked") if @table.status == "available" && @table.orders.last.status == "pending"
   end
 end
