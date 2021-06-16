5c9af1beaeea8cd403eb9d2444976e7159b956ac
Pierre-Guillaume <80978648+PGmiel@users.noreply.github.com>
2021-06-16 22:03:35 +0200

    Issue not fixed but identfied

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,16 +19,6 @@ class OrdersController < ApplicationController
     @wines = @restaurant.wines
   end
 
-  # def closing_table
-  #   # @order = Order.find(params[:order_id])
-  #   # @pending_orders = @order.order_items.select { |order_item| order_item.status == "sent" }
-  #   # @pending_orders.each { |pending_order| pending_order.update(status: "paid") }
-  #   # @table = Table.find(@order.table_id)
-  #   # @order.update(status: "paid")
-  #   # @order.table.update(status: "available")
-  #   # redirect_to thank_you_path
-  # end
-
   def create_order
     @table = Table.find(params[:table_id])
     @order = Order.create(table: @table, status: "pending")
