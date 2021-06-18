142ebdd628d95ff6bfa420431497ac483300b393
Roch de Vidts <roch-dv@hotmail.com>
2021-06-18 17:08:07 +0200

    anchor on the show order is smoother

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   if (document.getElementById("banner-typed-text")) {
     new Typed('#banner-typed-text', {
-      strings: ["Book a restaurant", "Find an empty table near you", "Book a terrasse seat"],
+      strings: ["Book a restaurant", "Find an empty table", "Book a terrasse seat"],
       typeSpeed: 54,
       loop: true
     });
